1ad2cf3751f5fac2a01e078c930c429fdeb031de
Akshyansu3479 <akshyansupritam7@gmail.com>
2024-08-28 12:45:00 +0530

    coonected-elasticach-for-redis

--- server/config/redis.js
+++ server/config/redis.js
@@ -2,6 +2,22 @@ const Redis = require("ioredis");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const redisClient = new Redis(process.env.REDIS_URL);
+const redisClient = Redis.createClient({
+    socket: {
+      host: "master.redis-url-shortener-rate-limiter.puaofd.aps1.cache.amazonaws.com:6379",
+      port: 6379
+    },
+    password: "AkshyansuPritam17",
+    username: "default"
+  });
 
 module.exports = redisClient;
+
+
+// const Redis = require("ioredis");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const redisClient = new Redis(process.env.REDIS_URL);
+
+// module.exports = redisClient;
