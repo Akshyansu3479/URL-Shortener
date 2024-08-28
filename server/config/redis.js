26669bd4a97c5c8e3932bb8013b0b76dee3e2eb1
Akshyansu3479 <akshyansupritam7@gmail.com>
2024-08-28 12:56:49 +0530

    redis-url-added-to-env

--- server/config/redis.js
+++ server/config/redis.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const redisClient = Redis.createClient({
     socket: {
-      host: "master.redis-url-shortener-rate-limiter.puaofd.aps1.cache.amazonaws.com:6379",
+      host: process.env.REDIS_URL,
       port: 6379
     },
     password: "AkshyansuPritam17",
